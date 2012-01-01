8e9f6579496282aa60788d7902615e7a8a54ce56
Thomas Pun <thomas.pun@me.com>
2012-01-01 14:10:26 -0800

    update params

--- generate_thumbnail_grid.rb
+++ generate_thumbnail_grid.rb
@@ -1,10 +1,9 @@
-require 'models/searcher'
+require_relative 'models/searcher.rb'
+require_relative 'models/search_presenter.rb'
 
 raise 'Need a search term' unless ARGV.count==1
 default_output = 'index.html'
 
 query = ARGV.first
-presenter = SearchPresenter.new(
-  :searcher => Searcher.new(query),
-  :fixed_width => 800)
+presenter = SearchPresenter.new(Searcher.new(query), 800)
 presenter.write_thumbnail_grid_html default_output
